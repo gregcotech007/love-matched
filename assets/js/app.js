ae04e2b0c79398e19a0a3eb07c05bfa34c725828
gregcotech007 <commins.greg@outlook.com>
2022-01-31 17:26:32 +0000

    Start game on click and increase flip counter on each card click

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,42 @@
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', ready());
-} else {
-    ready();
+class LoveMatched {
+    constructor(timeTotal, cards) {
+        this.cardsArray = cards;
+        this.timeTotal = timeTotal;
+        this.timeRem = timeTotal;
+        this.timer = document.getElementById('timeRem');
+        this.ticker = document.getElementById('flipCount');
+    }
+    gameStart() {
+        this.checkCard = null;
+        this.clicksTotal = 0;
+        this.timeRem = this.timeTotal;
+        this.cardsMatched = [];
+        this.busy = true;
+
+
+    }
+
+    cardFlip(card) {
+        if (this.yesCardFlip(card)) {
+            this.clicksTotal++;
+            this.ticker.innerText = this.clicksTotal;
+            card.classList.add('visible');
+
+            //if statement
+        }
+    }
+
+
+    yesCardFlip(card) {
+        return true;
+        //return !this.busy && !this.matchedCards.includes(card) && card != this.cardToCheck;
+    }
 }
 
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('text-overlay'));
-    let cards = Array.from(document.getElementsByClassName('cards'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new LoveMatched(90, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
@@ -17,7 +47,13 @@ function ready() {
     cards.forEach(card => {
         card.addEventListener('click', () => {
             game.cardFlip(card);
-        });
-    });
+        })
+    })
 }
 
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+}
+else {
+    ready();
+}

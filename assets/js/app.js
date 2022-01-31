0ab61e3546bd6bbed60f18035c17ecdf0cba8930
gregcotech007 <commins.greg@outlook.com>
2022-01-31 18:45:46 +0000

    Initiate countdown time and reaches zero, call gameOver function with text

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,21 @@ class LoveMatched {
         this.cardsMatched = [];
         this.busy = true;
 
-        
+        setTimeout(() => {
+            this.cardsShuffle();
+            this.countdownTimer = this.startCountdownTimer();
+            this.busy = false;
+        }, 500);
+        this.hideCards();
+        this.timer.innerText = this.timeRem;
+        this.ticker.innerText = this.clicksTotal;
+    }
+
+    hideCards() {
+        this.cardsArray.forEach(card => {
+            card.classList.remove('visible');
+            card.classList.remove('matched');
+        })
     }
 
     cardFlip(card) {
@@ -25,6 +39,21 @@ class LoveMatched {
             //if statement
         }
     }
+
+    startCountdownTimer() {
+        return setInterval(() => {
+            this.timeRem--;
+            this.timer.innerText = this.timeRem;
+            if (this.timeRem === 0)
+                this.gameOver();
+        }, 1000);
+    }
+
+    gameOver() {
+        clearInterval(this.countdownTimer);
+        document.getElementById('text-GameOver').classList.add('visible');
+    }
+
     // Fisher Yates Shuffle Algorithm
     cardsShuffle() {
         for (let i = this.cardsArray.length -1; i > 0; i--) {
@@ -43,7 +72,7 @@ class LoveMatched {
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('text-overlay'));
     let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new LoveMatched(90, cards);
+    let game = new LoveMatched(5, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
